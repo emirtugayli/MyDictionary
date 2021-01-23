a4f74657004da0b4ca0b0304bbaf77de246c5c54
emirtugayli <emirtugayli03@gmail.com>
2021-01-23 19:00:36 +0300

    4.gün ödevi

---
+++ MyDictionary.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryHomework
+{
+    class MyDictionary<TKey, TValue>
+    {
+        private TKey[] _key;
+        private TKey[] _tempKey;
+        private TValue[] _value;
+        private TValue[] _tempValue;
+
+
+        //Constructor olusturdum.
+        public MyDictionary()
+        {
+            _key = new TKey[0];
+            _value = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            //Keyleri Temp Arraydan Main Arraya geri aktariyoruz
+            _tempKey = _key;
+            _key = new TKey[_key.Length + 1];
+            for (int i = 0; i < _tempKey.Length; i++)
+            {
+                _key[i] = _tempKey[i];
+            }
+
+
+            //Valueleri Temp Arrayden Main Arraya Geri Aktariyoruz.
+            _tempValue = _value;
+            _value = new TValue[_value.Length + 1];
+            for (int i = 0; i < _tempValue.Length; i++)
+            {
+                _value[i] = _tempValue[i];
+            }
+
+            _key[_key.Length - 1] = key;
+            _value[_value.Length - 1] = value;
+        }
+
+        public int Count()
+        {
+            return _key.Length;
+        }
+
+        public TKey GetKey(int index)
+        {
+            return _key[index];
+        }
+
+        public TValue GetValue(int index)
+        {
+            return _value[index];
+        }
+        public TValue GetValueByKey(TKey key)
+        {
+            for (int i = 0; i < _key.Length; i++)
+            {
+                if (key.Equals(_key[i]))
+                {
+                    return _value[i];
+                }
+            }
+            throw new Exception("Boyle bir key(Ogrenci No) bulunmamaktadir.");
+        }
+
+        public TKey GetKeyByValue(TValue value)
+        {
+            for (int i = 0; i < _value.Length; i++)
+            {
+                if (value.Equals(_value[i]))
+                {
+                    return _key[i];
+                }
+            }
+            throw new Exception("Boyle bir value(ogrenci ismi) bulunmamaktadir.");
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionaryHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> sinifListesi = new MyDictionary<int, string>();
+            sinifListesi.Add(143 , "Mehmet");
+            sinifListesi.Add(153 , "Ayse");
+            sinifListesi.Add(764 , "Mete");
+            sinifListesi.Add(199 , "Bahar");
+            sinifListesi.Add(415 , "Emir");
+            sinifListesi.Add(73, "Yusa");
+
+
+
+            //
+            for (int i = 0; i < sinifListesi.Count(); i++)
+            {
+                Console.WriteLine(sinifListesi.GetValue(i));
+            }
+            //
+            try
+            {
+                Console.WriteLine("Ogrenci No Giriniz: ");
+                int key;
+                key = int.Parse(Console.ReadLine());
+                for (int i = 0; i < sinifListesi.Count(); i++)
+                {
+                    if (key == sinifListesi.GetKey(i))
+                    {
+                        string print = sinifListesi.GetValueByKey(key);
+                        Console.WriteLine(print);
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+            }
+            //
+            try
+            {
+                Console.WriteLine("Ogrenci ismi giriniz: ");
+                string key;
+                key = Console.ReadLine();
+                for (int i = 0; i < sinifListesi.Count(); i++)
+                {
+                    if (key == sinifListesi.GetValue(i))
+                    {
+                        var print = sinifListesi.GetKeyByValue(key);
+                        Console.WriteLine(print);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex);
+            }
+            Console.WriteLine($"Sınıf Mevcudunuz: {sinifListesi.Count()}");
+                
+            
+           
+
+           
+       
+
+        }
+    }
+}
